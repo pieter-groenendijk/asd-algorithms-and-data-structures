99a64e89db8c4afed9b2fe632a0fc66ad7bb461f
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-12-25 17:04:11 +0100

    Did some fun mergesort optimizing: now merges subarrays in opposite order to minimize boundary checks

--- source/sorting/mergesort/mergesort.go
+++ source/sorting/mergesort/mergesort.go
@@ -6,72 +6,141 @@ import (
 
 func Sort[T cmp.Ordered](list []T) []T {
 	listLength := len(list)
+	if listLength <= 1 {
+		return list
+	}
 
 	readFrom := list
 	writeTo := make([]T, listLength)
+	writeTo[listLength - 1] = readFrom[listLength - 1]
 
-	if listLength % 2 != 0 { // uneven
-		writeTo[listLength - 1] = readFrom[listLength - 1]
-	}
-
-	var leftStartAt int
-	var rightStartAt int
-	var rightEndedAt int 
-
-	var nextLeftAt int
-	var nextRightAt int
-	var insertAt int
-
-	partLength := 1
-	mergeLength := 2
-	for partLength < listLength {
-		leftStartAt = 0
-		rightStartAt = leftStartAt + partLength
-		rightEndedAt = min(mergeLength, listLength)
-		insertAt = 0
-		for rightStartAt < listLength {
-			nextLeftAt = leftStartAt
-			nextRightAt = rightStartAt
-
-			// merge items
-			for nextLeftAt < rightStartAt && nextRightAt < rightEndedAt {
-				if readFrom[nextLeftAt] < readFrom[nextRightAt] {
-					writeTo[insertAt] = readFrom[nextLeftAt]
-
-					nextLeftAt++
+	pairSize := 2
+	pyramidSize := 4
+	for pairSize <= listLength { // horizontal iterations
+		// We mergesort every EVEN pair LEFT-TO-RIGHT
+		leftAt := 0
+		rightAt := pairSize - 1
+		writeAt := leftAt
+		for rightAt < listLength {
+			// start merging this pair
+			for leftAt != rightAt {
+				if readFrom[leftAt] < readFrom[rightAt] {
+					writeTo[writeAt] = readFrom[leftAt]
+					leftAt++
 				} else {
-					writeTo[insertAt] = readFrom[nextRightAt]
-
-					nextRightAt++
+					writeTo[writeAt] = readFrom[rightAt]
+					rightAt--
 				}
 
-				insertAt++
+				writeAt++
 			}
-			// insert leftover `left` items if there are any
-			for ; nextLeftAt < rightStartAt; nextLeftAt++ {
-				writeTo[insertAt] = readFrom[nextLeftAt]
-				insertAt++
-			}
-			// insert leftover `right` items if there are any
-			for ; nextRightAt < rightEndedAt; nextRightAt++ {
-				writeTo[insertAt] = readFrom[nextRightAt]
-				insertAt++
-			}
+			writeTo[writeAt] = readFrom[leftAt]
 
-			// prepare for next iteration
-			leftStartAt += mergeLength
-			rightStartAt += mergeLength
-			rightEndedAt = min(rightEndedAt+mergeLength, listLength)
+			// already move to next iteration's pair
+			leftAt += pyramidSize
+			rightAt += pyramidSize
 		}
 
-		// prepare for next merge iteration
-		partLength += partLength
-		mergeLength += mergeLength
 
-		readFrom, writeTo = writeTo, readFrom
+		// We mergesort every UNEVEN pair RIGHT-TO-LEFT
+		rightAt = pyramidSize - 1
+		writeAt = rightAt
+		for rightAt < listLength {
+			// start merging this pair
+			for leftAt != rightAt {
+				if readFrom[leftAt] < readFrom[rightAt] {
+					writeTo[writeAt] = readFrom[leftAt]
+					leftAt++
+				} else {
+					writeTo[writeAt] = readFrom[rightAt]
+					rightAt--
+				}
+
+				writeAt++
+			}
+			writeTo[writeAt] = readFrom[leftAt]
+
+			// already move to next iteration's pair
+			leftAt += pyramidSize
+			rightAt += pyramidSize
+		}
+
+		// already prepare for next iteration
+		pairSize += pairSize
+		pyramidSize += pyramidSize
+		writeTo, readFrom = readFrom, writeTo
 	}
 
+	// If it's the case, add the remaining item which could not be paired in the first horizontal iteration
+
 	return readFrom
+
+	// Horizontal linear iteration of 
+		// We sort every even pair from left to right
+			// merge as said
+		// We sort every uneven pair from right to left
+			// merge as said
+
+
+
+	// var leftStartAt int
+	// var rightStartAt int
+	// var rightEndedAt int 
+
+	// var nextLeftAt int
+	// var nextRightAt int
+	// var insertAt int
+
+	// partLength := 1
+	// mergeLength := 2
+	// for partLength < listLength {
+	// 	leftStartAt = 0
+	// 	rightStartAt = leftStartAt + partLength
+	// 	rightEndedAt = min(mergeLength, listLength)
+	// 	insertAt = 0
+	// 	for rightStartAt < listLength {
+	// 		nextLeftAt = leftStartAt
+	// 		nextRightAt = rightStartAt
+
+	// 		// merge items
+	// 		for nextLeftAt < rightStartAt && nextRightAt < rightEndedAt {
+	// 			if readFrom[nextLeftAt] < readFrom[nextRightAt] {
+	// 				writeTo[insertAt] = readFrom[nextLeftAt]
+
+	// 				nextLeftAt++
+	// 			} else {
+	// 				writeTo[insertAt] = readFrom[nextRightAt]
+
+	// 				nextRightAt++
+	// 			}
+
+	// 			insertAt++
+	// 		}
+	// 		// insert leftover `left` items if there are any
+	// 		for ; nextLeftAt < rightStartAt; nextLeftAt++ {
+	// 			writeTo[insertAt] = readFrom[nextLeftAt]
+	// 			insertAt++
+	// 		}
+	// 		// insert leftover `right` items if there are any
+	// 		for ; nextRightAt < rightEndedAt; nextRightAt++ {
+	// 			writeTo[insertAt] = readFrom[nextRightAt]
+	// 			insertAt++
+	// 		}
+
+	// 		// prepare for next iteration
+	// 		leftStartAt += mergeLength
+	// 		rightStartAt += mergeLength
+	// 		rightEndedAt = min(rightEndedAt+mergeLength, listLength)
+	// 	}
+
+	// 	// prepare for next merge iteration
+	// 	partLength += partLength
+	// 	mergeLength += mergeLength
+
+	// 	readFrom, writeTo = writeTo, readFrom
+	// }
+
+	// return readFrom
 }
 
 
