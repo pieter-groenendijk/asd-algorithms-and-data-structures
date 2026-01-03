6b062e7fedfbc2ced1dca91a31c81c1f2a68ae25
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2026-01-03 13:00:00 +0100

    Binary search w/o bugs

--- source/binsearch/binsearch.go
+++ source/binsearch/binsearch.go
@@ -2,19 +2,24 @@ package binsearch
 
 import (
 	"cmp"
+
 	"github.com/pieter-groenendijk/asd-algorithms-and-data-structures/collections"
 )
 
-func search[TValue cmp.Ordered](values []TValue, value TValue) (int, error) {
-	leftAt := 0
+func Search[TValue cmp.Ordered](values []TValue, value TValue) (int, error) {
 	rightAt := len(values) - 1
+	if rightAt < 0 {
+		return -1, collections.ErrNotFound
+	}
+
+	leftAt := 0
 	var middleAt int
 	for leftAt != rightAt {
-		middleAt = rightAt - leftAt / 2 
+		middleAt = (rightAt - leftAt) / 2 + leftAt
 		if value > values[middleAt] {
 			leftAt = middleAt + 1
 		} else { // value <= values[middleAt]
-			// we assume value < values[middleAt], to prevent an repeated check, and 
+			// we assume value < values[middleAt], to prevent an repeated check, and
 			// instead rely on our loop boundary condition.
 			rightAt = middleAt
 		}
---
+++ source/binsearch/binsearch_test.go
@@ -0,0 +1,127 @@
+package binsearch
+
+import (
+	"testing"
+
+	"github.com/pieter-groenendijk/asd-algorithms-and-data-structures/collections"
+	"github.com/pieter-groenendijk/asd-algorithms-and-data-structures/testutils"
+)
+
+func intPointer(i int) *int {
+	return &i
+}
+
+func TestSearch(t *testing.T) {
+	testCases := []struct{
+		name string
+		giveValues []int
+		giveValue int
+		expectIndexIn []int 
+		expectErr error
+	}{
+		{
+			name: "valueNotInZeroValues",
+			giveValues: []int{},
+			giveValue: 5,
+			expectIndexIn: nil,
+			expectErr: collections.ErrNotFound,
+		},
+		{
+			name: "valueNotInOneValue",
+			giveValues: []int{8},
+			giveValue: 5,
+			expectIndexIn: nil,
+			expectErr: collections.ErrNotFound,
+		},
+		{
+			name: "valueNotInTwoValues",
+			giveValues: []int{5,7},
+			giveValue: 6,
+			expectIndexIn: nil,
+			expectErr: collections.ErrNotFound,
+		},
+		{
+			name: "valueNotInThreeValues",
+			giveValues: []int{5,7,9},
+			giveValue: 8,
+			expectIndexIn: nil,
+			expectErr: collections.ErrNotFound,
+		},
+		{
+			name: "valueNotInDuplicateValues",
+			giveValues: []int{5,5,5},
+			giveValue: 8,
+			expectIndexIn: nil,
+			expectErr: collections.ErrNotFound,
+		},
+
+		{
+			name: "valueInOneValue",
+			giveValues: []int{5},
+			giveValue: 5,
+			expectIndexIn: []int{0},
+			expectErr: nil,
+		},
+		{
+			name: "valueInTwoValuesAtStart",
+			giveValues: []int{5,7},
+			giveValue: 5,
+			expectIndexIn: []int{0},
+			expectErr: nil,
+		},
+		{
+			name: "valueInTwoValuesAtEnd",
+			giveValues: []int{6,8},
+			giveValue: 8,
+			expectIndexIn: []int{1},
+			expectErr: nil,
+		},
+		{
+			name: "valueInThreeValuesAtStart",
+			giveValues: []int{3,4,8},
+			giveValue: 3,
+			expectIndexIn: []int{0},
+			expectErr: nil,
+		},
+		{
+			name: "valueInThreeValuesAtEnd",
+			giveValues: []int{4,5,8},
+			giveValue: 8,
+			expectIndexIn: []int{2},
+			expectErr: nil,
+		},
+		{
+			name: "valueInThreeValuesAtMiddle",
+			giveValues: []int{3,4,5},
+			giveValue: 4,
+			expectIndexIn: []int{1},
+			expectErr: nil,
+		},
+		{
+			name: "valueInDuplicateValues",
+			giveValues: []int{3,3,4,4,4,4,7,8},
+			giveValue: 4,
+			expectIndexIn: []int{2,3,4},
+			expectErr: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			gotIndex, gotErr := Search(testCase.giveValues, testCase.giveValue)
+
+			if testCase.expectIndexIn != nil {
+				gotIndexIn := false
+				for _, expectIndex := range testCase.expectIndexIn {
+					if expectIndex == gotIndex {
+						gotIndexIn = true
+					}
+				}
+
+				testutils.AssertEquals(t, gotIndexIn, true)
+			}
+
+			testutils.AssertEquals(t, testCase.expectErr, gotErr)
+		})
+	}
+}
